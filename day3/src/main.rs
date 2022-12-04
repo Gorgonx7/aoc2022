bb04ecea3d15c4f5830195dedb6cb3cdb049c971
James Gordon <James.Gordon1628@gmail.com>
2022-12-04 17:18:10 +0000

    day 3 part 2

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -1,32 +1,36 @@
-use std::fs;
+use std::{fs, collections::HashMap};
 fn main() {
     let contents = fs::read_to_string("./input.txt")
     .expect("Should have been able to read the file");
     //println!("{}", contents);
-    let lines= contents.split("\n");
+    let lines: Vec<&str> = contents.split("\n").collect();
     let mut total = 0;
-    for s in lines {
-        let (first, second) = split_string_in_half(s);
-        let result = get_same_character(first, second);
-        total += get_character_value(result)
-        
+    let mut x = 0;
+    while x < lines.len(){
+        let result = get_same_character(lines[x], lines[x+1], lines[x+2]);
+        total += get_character_value(result);
+        x += 3;
     }
     println!("Total: {}",total)
 }
 
-fn split_string_in_half(to_split: &str) -> (&str, &str) {
-    let first = &to_split[..to_split.chars().count()/2];
-    let second = &to_split[to_split.chars().count()/2..];
-    return (first, second)
-}
+
+fn get_same_character(first_slot: &str, second_slot: &str,third_slot: &str) -> char{
+    let mut first_dictionary = HashMap::new();
+    let mut second_dictionary = HashMap::new();
 
 
-fn get_same_character(first_slot: &str, second_slot: &str) -> char{
     for first_char in first_slot.chars() {
-        for second_char in second_slot.chars() {
-            if first_char == second_char{
-                return first_char
-            }
+        first_dictionary.insert(first_char, true);   
+    }
+    for second_char in second_slot.chars() {
+       if first_dictionary.contains_key(&second_char){
+            second_dictionary.insert(second_char, true);
+       }
+    }
+    for third_char in third_slot.chars() {
+        if second_dictionary.contains_key(&third_char){
+            return third_char
         }
     }
     'a'
@@ -41,24 +45,16 @@ fn get_character_value(char_in: char) -> i32 {
 
 #[cfg(test)]
 mod tests {
+    use crate::{get_same_character, get_character_value};
 
-    use crate::{get_same_character, get_character_value, split_string_in_half};
-    #[test]
-    fn test_split_string_in_half(){
-        let input = "ABCD";
-        let want1 = "AB";
-        let want2= "CD";
-        let (got1, got2) = split_string_in_half(input);
-        assert_eq!(want1, got1);
-        assert_eq!(want2, got2);
-    }
+
     #[test]
     fn test_get_same_character(){
         let first_input = "ABcDE";
         let second_input = "eFGHc";
-        
+        let third_input = "IJcKLMNO";
         let want = 'c';
-        let got = get_same_character(first_input, second_input);
+        let got = get_same_character(first_input, second_input, third_input);
         assert_eq!(want, got);
     }
     #[test]

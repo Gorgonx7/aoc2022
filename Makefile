455dc6bd8bc695ff5fcac1f588da34cbb0c99ece
James Gordon <james.gordon2@ibm.com>
2022-12-08 11:23:44 +0000

    add day 6

--- Makefile
+++ Makefile
@@ -3,9 +3,13 @@ build:
 	cd day2 && cargo build
 	cd day3 && cargo build
 	cd day4 && cargo build
+	cd day5 && cargo build
+	cd day6 && cargo build
 
 test:
 	cd day1 && cargo test
 	cd day2 && cargo test
 	cd day3 && cargo test
 	cd day4 && cargo test
+	cd day5 && cargo test
+	cd day6 && cargo test
--- day6/src/main.rs
+++ day6/src/main.rs
@@ -1,3 +1,36 @@
+use std::{collections::HashMap, fs};
+
 fn main() {
-    println!("Hello, world!");
+    let contents = fs::read_to_string("./input.txt").expect("Unable to open");
+    let char_vec: Vec<char> = contents.chars().collect();
+    let mut i = 13;
+
+    while i < char_vec.len() {
+        i += 1;
+        let mut dictionary: HashMap<char, bool> = HashMap::new();
+        dictionary.insert(char_vec[i - 14], true);
+        dictionary.insert(char_vec[i - 13], true);
+        dictionary.insert(char_vec[i - 12], true);
+        dictionary.insert(char_vec[i - 11], true);
+        dictionary.insert(char_vec[i - 10], true);
+        dictionary.insert(char_vec[i - 9], true);
+        dictionary.insert(char_vec[i - 8], true);
+        dictionary.insert(char_vec[i - 7], true);
+        dictionary.insert(char_vec[i - 6], true);
+        dictionary.insert(char_vec[i - 5], true);
+        dictionary.insert(char_vec[i - 4], true);
+        dictionary.insert(char_vec[i - 3], true);
+        dictionary.insert(char_vec[i - 2], true);
+        dictionary.insert(char_vec[i - 1], true);
+        if dictionary.len() == 14 {
+            break;
+        }
+    }
+    println!("{}", i);
+}
+
+#[cfg(test)]
+mod test {
+    #[test]
+    fn test_detect_pattern() {}
 }
